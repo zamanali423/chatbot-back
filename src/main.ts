d9fe543806b7cb95aedf868183a7bb4dfdacf6c8
Zaman Ali <zamanyaseen2024@gmail.com>
2025-09-18 19:39:38 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -21,4 +21,7 @@ async function bootstrap() {
   await app.listen(process.env.PORT ?? 8080);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
-bootstrap();
+if (process.env.NODE_ENV !== 'production') {
+  bootstrap();
+}
+
